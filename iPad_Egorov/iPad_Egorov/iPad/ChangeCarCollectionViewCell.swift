233ef819db252836fc3419673a00cdb6d987e79d
Жегев Реев <93246335+TokyoBWR@users.noreply.github.com>
2023-04-03 11:22:28 +0300

    Add files via upload

---
+++ iPad_Egorov/iPad_Egorov/iPad/ChangeCarCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ChangeCarCollectionViewCell.swift
+//  Kanavichev drive
+//
+//  Created by Канавичев Алексей Александрович on 23/1/2023.
+//
+
+import UIKit
+
+
+class ChangeCarCollectionViewCell: UICollectionViewController {
+    
+    @IBOutlet weak var carPicture: UIImageView!
+
+
+}
---
+++ iPad_Egorov/iPad_Egorov/iPad/ResultTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ResultTableViewCell.swift
+//  Drive
+//
+//  Created by Egorov Aleksey on 16/1/2023.
+//
+
+import UIKit
+
+class ResultTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var namePlayer: UILabel!
+    @IBOutlet weak var gameResult: UILabel!
+    @IBOutlet weak var timeResult: UILabel!
+    
+   
+}
